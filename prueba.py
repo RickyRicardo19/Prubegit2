1b0bef806ddb229f96d4c1efb4a609f99b28596b
RickyRicardo19 <119453511+RickyRicardo19@users.noreply.github.com>
2022-12-12 22:08:52 -0300

    una vez mas

---
+++ prueba.py
@@ -0,0 +1,8 @@
+class  test:
+    def __init__(self):
+        self.variable = 'old'
+        self.Change(self.variable)
+    def Change(self.var):
+        var ='New'
+object=test()
+print(object.variable)
